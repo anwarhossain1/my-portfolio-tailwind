c65ae572f7a133f903f571c50e1566e8e6af32c3
@anwarhossain1 <irishkhan33@gmail.com>
2023-03-24 17:50:07 +0600

    hero section added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import HeroSection from "../src/components/HeroSection";
 import Navbar from "../src/components/Navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+        <HeroSection />
+      </main>
     </>
   );
 }
