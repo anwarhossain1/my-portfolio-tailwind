22b36970db2f5ac4d19cd53cee7c8fa475839b2c
@anwarhossain1 <irishkhan33@gmail.com>
2023-05-19 16:53:46 +0600

    social links added

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,10 +1,30 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { HiArrowDown } from "react-icons/hi";
 import { Link } from "react-scroll/modules";
+import github from "../../assets/social/github.png";
 import profilepic from "./../../assets/profile.jpg";
+import fb from "./../../assets/social/fb.png";
+import linkedin from "./../../assets/social/linkedin.png";
+
+const socialLinks = [
+  {
+    image: github,
+    link: "http://github.com/anwarhossain1",
+  },
+  {
+    image: linkedin,
+    link: "https://www.linkedin.com/in/anwarhossain1/",
+  },
+  {
+    image: fb,
+    link: "https://www.facebook.com/anwarirish",
+  },
+];
 
 const HeroSection = () => {
+  const router = useRouter();
   return (
     <section id="home">
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
@@ -21,7 +41,7 @@ const HeroSection = () => {
           <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">
             Hi, I&#39;m Anwar Hossain!
           </h1>
-          <p className="text-lg mt-4 mb-6 md:text-2xl">
+          <p className="text-lg mt-4 mb-5 md:text-2xl">
             I&#39;m a{" "}
             <span className="font-semibold text-teal-600">
               Professional React Developer{" "}
@@ -29,9 +49,24 @@ const HeroSection = () => {
             based in Dhaka, Bangladesh. Working towards creating web
             applications that makes life easier and more meaningful.
           </p>
+          <div className="flex flex-row items-center justify-center md:justify-start gap-4 mb-10 ">
+            {socialLinks.map((item, index) => {
+              return (
+                <Image
+                  src={item.image}
+                  alt=""
+                  width={40}
+                  height={40}
+                  className="rounded-full shadow-xl cursor-pointer"
+                  key={index}
+                  onClick={() => window.open(item.link, "_ blank")}
+                />
+              );
+            })}
+          </div>
           <Link
             to="projects"
-            className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
+            className="cursor-pointer text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
             activeClass="active"
             spy={true}
             smooth={true}
